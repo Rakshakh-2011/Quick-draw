3a04ab4f40649ac3b1e8b7dae6177991dc9827de
Rakshakh-2011 <78683334+Rakshakh-2011@users.noreply.github.com>
2021-10-23 13:50:17 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -2,10 +2,34 @@ quick_draw_data_set = ["aircraft carrier", "airplane", "alarm clock", "ambulance
 
 function setup() {
     canvas = createCanvas(280, 280);
-    canvas.center();
-    background("white");
+     canvas.center();
+      background("white"); 
+      canvas.mouseReleased(classifyCanvas); 
+      synth = window.speechSynthesis;
+    } 
+    function preload() {
+         classifier = ml5.imageClassifier('DoodleNet'); 
+       }
+       function clearCanvas() { 
+           background("white"); 
+       }
+        function draw() {
+              strokeWeight(13); 
+               stroke("black");
+                if (mouseIsPressed) {
+                     line(pmouseX, pmouseY, mouseX, mouseY); 
+                   } 
+               }
+function classifyCanvas() {
+   classifier.classify(canvas, gotResult);
 }
-
-function clearCanvas(){
-    background("white");
+function gotResult(error, results) {
+   if (error) {
+       console.log(error);
+   }
+   console.log(results);
+   document.getElementById('label').innerHTML = 'Label: ' + results[0].label;
+   document.getElementById('confidence').innerHTML = 'Confidence: ' + Math.round(results[0].confidence * 100) + '%';
+   utterThis = new SpeechSynthesisUtterance(results[0].label);
+   synth.speak(utterThis);
 }
